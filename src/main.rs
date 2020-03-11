c61e4084bf5949285c986f38131b61212e34be32
Sergio de Carvalho <scarvalhojr@gmail.com>
2020-03-11 23:25:37 +0000

    A minimal Rust kernel

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-#![no_std]
-#![no_main]
+#![no_std] // Don't link the Rust standard library
+#![no_main] // Disable all Rust-level entry points
 
 use core::panic::PanicInfo;
 
@@ -9,7 +9,19 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+static HELLO: &[u8] = b"Welcome to Ferrox";
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
+
